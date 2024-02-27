e85bd694a0307fea3bf52230c22fe9190791b717
Opilane TTHK <opilane@tthk.ee>
2024-02-27 14:22:52 +0200

    Create sthfhwrsuyh.cs

---
+++ sthfhwrsuyh.cs
@@ -0,0 +1,25 @@
+﻿namespace pekivahe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int[] numbers = { 2, 4, 6, 8, 10 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            Console.WriteLine("-----------------------");
+
+            for (int i = 0;i < numbers.Length; i++)
+            {
+                int plus = numbers[i] + 10;
+                Console.WriteLine(plus);
+            }
+        }
+    }
+}
